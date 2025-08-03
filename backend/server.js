4c34fc0cd3bad8a599cf0a9efe1eb2011b29011b
Omkaar <omdb2004@gmail.com>
2025-08-03 22:32:11 +0530

    T-9

--- backend/server.js
+++ backend/server.js
@@ -9,13 +9,29 @@ const PORT = process.env.PORT || 5001;
 
 async function initDB() {
   try {
-    await sql;
-  } catch (error) {}
+    await sql`
+      CREATE TABLE IF NOT EXISTS transactions (
+        id SERIAL PRIMARY KEY,
+        user_id VARCHAR(255) NOT NULL,
+        title VARCHAR(255) NOT NULL,
+        amount DECIMAL(10,2) NOT NULL,
+        category VARCHAR(255) NOT NULL,
+        created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+      )
+    `;
+    console.log("✅ Database initialized successfully");
+  } catch (error) {
+    console.error("❌ Error initializing DB:", error);
+    process.exit(1); // 1 = failure
+  }
 }
+
 app.get("/", (req, res) => {
   res.send("it's working");
 });
 
-app.listen(PORT, () => {
-  console.log(`server is running on ${PORT}`);
+initDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`🚀 Server is running on http://localhost:${PORT}`);
+  });
 });
