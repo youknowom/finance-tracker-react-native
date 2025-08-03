9317f9dc708f70582326cfddb2748aaf966a5fe0
Omkaar <omdb2004@gmail.com>
2025-08-03 22:24:02 +0530

    T-8

---
+++ backend/config/db.js
@@ -0,0 +1,5 @@
+import { neon } from "@neondatabase/serverless";
+
+import "dotenv/config";
+
+export const sql = neon(process.env.DATABASE_URL);
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
+import { sql } from "./config/db.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
 
+async function initDB() {
+  try {
+    await sql;
+  } catch (error) {}
+}
 app.get("/", (req, res) => {
   res.send("it's working");
 });
