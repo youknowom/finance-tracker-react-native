366ba00f3f4d59f68f21c4028ab2e89b8a1dc452
Omkaar <omdb2004@gmail.com>
2025-08-03 22:04:26 +0530

    T-5

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,10 @@
 import express from "express";
+
 const app = express();
+
+app.get("/", (req, res) => {
+  res.send("it's working");
+});
 
 app.listen(5001, () => {
   console.log("server is running on 5001");
