4bd201e0af167e4a54db3c3b6883ed8377e7e7ac
Omkaar <omdb2004@gmail.com>
2025-08-04 20:21:35 +0530

    T-32

---
+++ mobile/components/SafeScreen.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const SafeScreen = () => {
+  return (
+    <View>
+      <Text>SafeScreen</Text>
+    </View>
+  );
+};
+
+export default SafeScreen;
