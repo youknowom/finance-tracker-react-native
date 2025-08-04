5d68306acc529806c77da27ae4b2db5e9aaf66ab
Omkaar <omdb2004@gmail.com>
2025-08-04 19:53:29 +0530

    T-29

--- mobile/app/index.jsx
+++ mobile/app/index.jsx
@@ -15,10 +15,7 @@ export default function Index() {
       <Text>Hello</Text>
       <Image
         style={{ width: 150, height: 150, borderRadius: 10 }}
-        source={{
-          uri: "https://images.unsplash.com/photo-1745776707319-c01d866ee6b6?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8",
-        }}
-        contentFit="cover"
+        source={require("@/assets/images/pfp.jpg")}
       />
     </View>
   );
