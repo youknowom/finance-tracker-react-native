1412b39837f7b302ceabe99de925d6c5eab720d4
Omkaar <omdb2004@gmail.com>
2025-08-04 20:31:39 +0530

    T-35

--- mobile/components/SafeScreen.jsx
+++ mobile/components/SafeScreen.jsx
@@ -1,8 +1,23 @@
 import { View, Text } from "react-native";
 import React from "react";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+
+import { COLORS } from "@/constants/colors";
 
 const SafeScreen = ({ children }) => {
-  return <View>{children}</View>;
+  const insets = useSafeAreaInsets();
+
+  return (
+    <View
+      style={{
+        paddingTop: insets.top,
+        flex: 1,
+        backgroundColor: COLORS.background,
+      }}
+    >
+      {children}
+    </View>
+  );
 };
 
 export default SafeScreen;
