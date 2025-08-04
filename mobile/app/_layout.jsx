4a1b356f0ceb144a929bdd4fbb5e45c3b3099a44
Omkaar <omdb2004@gmail.com>
2025-08-04 20:23:54 +0530

    T-33

--- mobile/app/_layout.jsx
+++ mobile/app/_layout.jsx
@@ -1,5 +1,10 @@
 import { Stack } from "expo-router";
+import SafeScreen from "@/components/SafeScreen";
 
 export default function RootLayout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  return (
+    <SafeScreen>
+      <Stack screenOptions={{ headerShown: false }} />
+    </SafeScreen>
+  );
 }
