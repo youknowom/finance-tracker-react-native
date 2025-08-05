9b51f86e3d000f4e785ae943085ed001905e313a
Omkaar <omdb2004@gmail.com>
2025-08-05 18:30:56 +0530

    T-45

--- mobile/app/(auth)/sign-in.jsx
+++ mobile/app/(auth)/sign-in.jsx
@@ -1,87 +1,11 @@
-// import { useSignIn } from "@clerk/clerk-expo";
-// import { Link, useRouter } from "expo-router";
-// import { Text, TextInput, TouchableOpacity, View } from "react-native";
-// import React from "react";
-// import { styles } from "../../assets/styles/auth.styles";
-
-// export default function Page() {
-//   const { signIn, setActive, isLoaded } = useSignIn();
-//   const router = useRouter();
-
-//   const [emailAddress, setEmailAddress] = React.useState("");
-//   const [password, setPassword] = React.useState("");
-
-//   // Handle the submission of the sign-in form
-//   const onSignInPress = async () => {
-//     if (!isLoaded) return;
-
-//     // Start the sign-in process using the email and password provided
-//     try {
-//       const signInAttempt = await signIn.create({
-//         identifier: emailAddress,
-//         password,
-//       });
-
-//       // If sign-in process is complete, set the created session as active
-//       // and redirect the user
-//       if (signInAttempt.status === "complete") {
-//         await setActive({ session: signInAttempt.createdSessionId });
-//         router.replace("/");
-//       } else {
-//         // If the status isn't complete, check why. User might need to
-//         // complete further steps.
-//         console.error(JSON.stringify(signInAttempt, null, 2));
-//       }
-//     } catch (err) {
-//       // See https://clerk.com/docs/custom-flows/error-handling
-//       // for more info on error handling
-//       console.error(JSON.stringify(err, null, 2));
-//     }
-//   };
-
-//   return (
-//     <View style={styles.container}>
-//       <Text>Sign in</Text>
-//       {error ? (
-//         <View style={styles.errorBox}>
-//           <Ionicons name="alert-circle" size={20} color={COLORS.expense} />
-//           <Text style={styles.errorText}>{error}</Text>
-//           <TouchableOpacity onPress={() => setError("")}>
-//             <Ionicons name="close" size={20} color={COLORS.textLight} />
-//           </TouchableOpacity>
-//         </View>
-//       ) : null}
-//       <TextInput
-//         autoCapitalize="none"
-//         value={emailAddress}
-//         placeholder="Enter email"
-//         onChangeText={(emailAddress) => setEmailAddress(emailAddress)}
-//       />
-//       <TextInput
-//         value={password}
-//         placeholder="Enter password"
-//         secureTextEntry={true}
-//         onChangeText={(password) => setPassword(password)}
-//       />
-//       <TouchableOpacity onPress={onSignInPress}>
-//         <Text>Continue</Text>
-//       </TouchableOpacity>
-//       <View style={{ display: "flex", flexDirection: "row", gap: 3 }}>
-//         <Link href="/sign-up">
-//           <Text>Sign up</Text>
-//         </Link>
-//       </View>
-//     </View>
-//   );
-// }
 import { useSignIn } from "@clerk/clerk-expo";
 import { Link, useRouter } from "expo-router";
-import { Text, TextInput, TouchableOpacity, View } from "react-native";
+import { Text, TextInput, TouchableOpacity, View, Image } from "react-native";
 import React from "react";
 import { Ionicons } from "@expo/vector-icons";
 import { COLORS } from "../../constants/colors";
 import { styles } from "../../assets/styles/auth.styles";
-
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 export default function Page() {
   const { signIn, setActive, isLoaded } = useSignIn();
   const router = useRouter();
@@ -112,7 +36,12 @@ export default function Page() {
   };
 
   return (
-    <View>
+    <KeyboardAwareScrollView
+      style={{ flex: 1 }}
+      contentContainerStyle={{ flexGrow: 1 }}
+      enableOnAndroid={true}
+      enableAutomaticScroll={true}
+    >
       <Text>Sign in</Text>
 
       <TextInput
@@ -138,6 +67,6 @@ export default function Page() {
           <Text>Sign up</Text>
         </Link>
       </View>
-    </View>
+    </KeyboardAwareScrollView>
   );
 }
