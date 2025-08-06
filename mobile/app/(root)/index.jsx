96ce489608853af487d3c7737ccbc20c8767034a
Omkaar <omdb2004@gmail.com>
2025-08-06 23:13:14 +0530

    T-57

--- mobile/app/(root)/index.jsx
+++ mobile/app/(root)/index.jsx
@@ -3,12 +3,15 @@ import { Link } from "expo-router";
 import { Text, View } from "react-native";
 import { SignOutButton } from "@/components/SignOutButton";
 import { useTransactions } from "../../hooks/useTransactions";
+import { useEffect } from "react";
 
 export default function Page() {
   const { user } = useUser();
   const { useTransactions, summary, isLoading, loading, deleteTransaction } =
     useTransactions(user.id);
-
+  useEffect(() => {
+    loadData();
+  }, [loadData]);
   return (
     <View>
       <SignedIn>
